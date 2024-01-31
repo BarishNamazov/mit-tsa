d05fa31fdeca587a128a4a1c5abb7da3dcc19715
Abutalib (Barish) Namazov <abutalib@mit.edu>
2024-01-31 17:01:01 +0400

    Draft

---
+++ data.js
@@ -0,0 +1,23 @@
+export const events = [
+  {
+    name: 'Our Spring 2024 Events',
+    description: 'Stay tuned for our upcoming events! Let us know if you have any ideas for events you would like to see in the future.',
+    time: 'Spring 2024',
+    location: 'MIT',
+  },
+  {
+    name: 'Turkish Students Dinner',
+    description: 'Come join us for dinner and get to know the members of the club!',
+    time: '6PM, February 5th, 2024',
+    location: 'Next House Dining Hall',
+
+  }
+];
+
+export const exec = [
+  'Deniz Güner (President)',
+  'Ege Kabasakaloğlu (Vice-President)',
+  'Şebnem Gül (Treasurer)',
+  'Berkin Binbaş (Social Chair)',
+  'Abutalib-Barish Namazov (Webmaster)',
+];
---
+++ index.js
@@ -0,0 +1,37 @@
+import { events, exec } from "./data.js";
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+
+const eventsList = $("#events-list");
+const eventTemplate = $("#event-template");
+
+events.forEach((event) => {
+  const eventNode = eventTemplate.content.cloneNode(true);
+  eventNode.querySelector(".event-name").textContent = event.name;
+  eventNode.querySelector(".event-description").textContent = event.description;
+  eventNode.querySelector(".event-time").textContent = event.time;
+  eventNode.querySelector(".event-location").textContent = event.location;
+  eventsList.appendChild(eventNode);
+});
+
+const execList = $("#exec-list");
+const execTemplate = $("#exec-template");
+
+exec.forEach((exec) => {
+  const execNode = execTemplate.content.cloneNode(true);
+  execNode.querySelector(".exec-name").textContent = exec;
+  execList.appendChild(execNode);
+});
+
+$("#mailing").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const data = new FormData(form);
+  const email = data.get("mailing_email");
+  const message = "\n" + data.get("mailing_message");
+  fetch(`/mailinglist.php?address=${encodeURIComponent(email)}&message=${encodeURIComponent(message)}`, {
+    method: 'GET',
+  });
+});
